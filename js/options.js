c9140beb4a8ab08dbc1f82eeb5d2ad4994ea29dc
GOGO98901 <gogo98901@live.co.uk>
2015-01-14 21:58:05 +0000

    Tried to get options working

--- js/options.js
+++ js/options.js
@@ -1,28 +1,31 @@
 var defaultState = "hide";
 
 function loadOptions() {
-  var tabs = localStorage["tabsRecent"];
+  chrome.storage.local.get('tabsRecent', function (result) {
+    tabs = result.tabsRecent;
+    var tabs = localStorage["tabsRecent"];
 
-  if (tabs == undefined || (tabs != "hide" && tabs != "show")) {
-    tabs = defaultState;
-  }
-
-  var select = document.getElementById("tabs");
-  for (var i = 0; i < select.children.length; i++) {
-    var child = select.children[i];
-    if (child.value == tabs) {
-      child.selected = "true";
-      break;
+    if (tabs == undefined || (tabs != "hide" && tabs != "show")) {
+      tabs = defaultState;
     }
-  }
-  console.log("Loaded Options tabs as :" + tabs)
+
+    var select = document.getElementById("tabs");
+    for (var i = 0; i < select.children.length; i++) {
+      var child = select.children[i];
+      if (child.value == tabs) {
+        child.selected = "true";
+        break;
+      }
+    }
+    console.log("Loaded Options tabs as :" + tabs)
+  });
 }
 
 function saveOptions() {
   var select = document.getElementById("tabs");
   var option = select.children[select.selectedIndex].value;
-  localStorage["tabsRecent"] = option;
-  console.log("Saved Options tabs as :" + option)
+  chrome.storage.local.set({'tabsRecent': option});
+  console.log("Saved Options tabs as :" + option);
 }
 
 function eraseOptions() {
