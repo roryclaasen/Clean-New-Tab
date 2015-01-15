0e1eb42f6f8332e6a94d5cf5a70ed151d4697a07
GOGO98901 <gogo98901@live.co.uk>
2015-01-15 17:04:58 +0000

    Added more options and got the plugin ready

--- js/background.js
+++ js/background.js
@@ -11,11 +11,23 @@ var url = window.location.href;
 if(url.startsWith("https://www.google.co.uk/_/")){
   url = url.replace("https://www.google.co.uk/_/","");
   if(url.startsWith("chrome/newtab")){
+    $('body').css('min-height', '0px');
     chrome.storage.local.get('tabsRecent', function (result) {
-      var  tabs = result.tabsRecent;
+      var tabs = result.tabsRecent;
       if (tabs == "hide") {
         $("#most-visited").remove();
-        console.log("removed");
+      }
+    });
+    chrome.storage.local.get('themeLogo', function (result) {
+      var theme = result.themeLogo;
+      if (theme == "hide") {
+        $("#theme-attr").remove();
+      }
+    });
+    chrome.storage.local.get('gplusBar', function (result) {
+      var gplusBar = result.gplusBar;
+      if (gplusBar == "hide") {
+        $("#mngb").remove();
       }
     });
   }
--- js/options.js
+++ js/options.js
@@ -3,11 +3,9 @@ var defaultState = "hide";
 function loadOptions() {
   chrome.storage.local.get('tabsRecent', function (result) {
     var  tabs = result.tabsRecent;
-
     if (tabs == undefined || (tabs != "hide" && tabs != "show")) {
       tabs = defaultState;
     }
-
     var select = document.getElementById("tabs");
     for (var i = 0; i < select.children.length; i++) {
       var child = select.children[i];
@@ -16,24 +14,60 @@ function loadOptions() {
         break;
       }
     }
-    console.log("Loaded Options tabs as :" + tabs)
+    console.log("Loaded Options tabs as :" + tabs);
+  });
+  chrome.storage.local.get('themeLogo', function (result) {
+    var  theme = result.tabsRecent;
+    if (theme == undefined || (theme != "hide" && theme != "show")) {
+      theme = defaultState;
+    }
+    var select = document.getElementById("theme");
+    for (var i = 0; i < select.children.length; i++) {
+      var child = select.children[i];
+      if (child.value == theme) {
+        child.selected = "true";
+        break;
+      }
+    }
+    console.log("Loaded Theme logo as :" + theme);
+  });
+  chrome.storage.local.get('gplusBar', function (result) {
+    var gplus = result.gplusBar;
+    if (gplus == undefined || (gplus != "hide" && gplus != "show")) {
+      gplus = defaultState;
+    }
+    var select = document.getElementById("gplus");
+    for (var i = 0; i < select.children.length; i++) {
+      var child = select.children[i];
+      if (child.value == gplus) {
+        child.selected = "true";
+        break;
+      }
+    }
+    console.log("Loaded Google+ bar as :" + gplus);
   });
 }
 
 function saveOptions() {
-  var select = document.getElementById("tabs");
-  var option = select.children[select.selectedIndex].value;
-  chrome.storage.local.set({'tabsRecent': option});
-  console.log("Saved Options tabs as :" + option);
+  var tabs = document.getElementById("tabs");
+  var tabsOption = tabs.children[tabs.selectedIndex].value;
+  chrome.storage.local.set({'tabsRecent': tabsOption});
+  console.log("Saved Options tabs as :" + tabsOption);
+
+  var theme = document.getElementById("theme");
+  var themeOption = theme.children[theme.selectedIndex].value;
+  chrome.storage.local.set({'themeLogo': themeOption});
+  console.log("Saved Theme logo as :" + themeOption);
+
+  var gplus = document.getElementById("gplus");
+  var gplusOption = gplus.children[gplus.selectedIndex].value;
+  chrome.storage.local.set({'gplusBar': gplusOption});
+  console.log("Saved Googe Plus Bar as :" + gplusOption);
 }
 
-function eraseOptions() {
-  localStorage.removeItem("tabsRecent");
-  location.reload();
-}
 function init() {
   loadOptions();
-  document.getElementById("tabs").addEventListener("change", saveOptions);
+  document.getElementById("options").addEventListener("change", saveOptions);
 }
 
 window.addEventListener("load", init);
